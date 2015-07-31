2aff2bd3c88b062dfee0f8edf5649a3dc043506e
Adam Sindelar <adam.sindelar@gmail.com>
2015-07-31 09:28:29 -0700

    Initial efilter import from Rekall.

---
+++ efilter/frontends/__init__.py
@@ -0,0 +1,6 @@
+"""EFILTER Forensic Query Language"""
+
+from efilter.frontends import experiments
+from efilter.frontends import lisp
+from efilter.frontends import literal
+from efilter.frontends import slashy
