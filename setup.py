7873ed2933a2be8ee79833c43502ef94bed4396a
Joachim Metz <joachim.metz@gmail.com>
2016-01-26 04:34:41 +0100

    Code review: 285220043: Changed setup.py to add python prefix.

--- setup.py
+++ setup.py
@@ -1,7 +1,88 @@
 #!/usr/bin/env python
 
+import sys
+
+try:
+  from setuptools import find_packages, setup, Command
+except ImportError:
+  from distutils.core import find_packages, setup, Command
+
+try:
+  from setuptools.commands.bdist_rpm import bdist_rpm
+except ImportError:
+  from distutils.command.bdist_rpm import bdist_rpm
+
+# Change PYTHONPATH to include efilter so that we can get the version.
+sys.path.insert(0, '.')
+
 from efilter import version
-from setuptools import find_packages, setup
+
+
+class BdistRPMCommand(bdist_rpm):
+  """Custom handler for the bdist_rpm command."""
+
+  def _make_spec_file(self):
+    """Generates the text of an RPM spec file.
+
+    Returns:
+      A list of strings containing the lines of text.
+    """
+    # Note that bdist_rpm can be an old style class.
+    if issubclass(BdistRPMCommand, object):
+      spec_file = super(BdistRPMCommand, self)._make_spec_file()
+    else:
+      spec_file = bdist_rpm._make_spec_file(self)
+
+    if sys.version_info[0] < 3:
+      python_package = 'python'
+    else:
+      python_package = 'python3'
+
+    description = []
+    summary = ''
+    in_description = False
+
+    python_spec_file = []
+    for index, line in enumerate(spec_file):
+      if line.startswith('Summary: '):
+        summary = line
+
+      elif line.startswith('BuildRequires: '):
+        line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+
+      elif line.startswith('Requires: '):
+        if python_package == 'python3':
+          line = line.replace('python', 'python3')
+
+      elif line.startswith('%description'):
+        in_description = True
+
+      elif line.startswith('%files'):
+        line = '%files -f INSTALLED_FILES -n {0:s}-%{{name}}'.format(
+           python_package)
+
+      elif line.startswith('%prep'):
+        in_description = False
+
+        python_spec_file.append(
+            '%package -n {0:s}-%{{name}}'.format(python_package))
+        python_spec_file.append('{0:s}'.format(summary))
+        python_spec_file.append('')
+        python_spec_file.append(
+            '%description -n {0:s}-%{{name}}'.format(python_package))
+        python_spec_file.extend(description)
+
+      elif in_description:
+        # Ignore leading white lines in the description.
+        if not description and not line:
+          continue
+
+        description.append(line)
+
+      python_spec_file.append(line)
+
+    return python_spec_file
+
 
 setup(name="efilter",
       version=version.get_version(),
@@ -12,6 +93,7 @@ setup(name="efilter",
       url="https://github.com/google/dotty/",
       packages=find_packages(exclude=["efilter_tests*"]),
       package_dir={"efilter": "efilter"},
+      cmdclass={'bdist_rpm': BdistRPMCommand},
       install_requires=[
           "python-dateutil > 2",
           "pytz >= 2011k"])
